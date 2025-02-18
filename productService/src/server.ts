20079e850be8f1aea1dec89038aacaebdfe6cecd
Ariel-Dayan <ariel0280@gmail.com>
2025-02-19 01:28:19 +0200

    use middlewares

--- productService/src/server.ts
+++ productService/src/server.ts
@@ -3,20 +3,28 @@ import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 
+import { config } from "./utils/config";
+import { notFoundMiddleware } from "./middlewares/notFound";
+import { errorHandlerMiddleware } from "./middlewares/errorHandler";
+
+
 export const createHttpServer = () => {
   const app = express();
 
   app.use(cookieParser());
   app.use(express.json());
   app.use(cors({ 
-      origin: "*"
+      origin: config.corsOrigin 
     })
   );
-  app.use(morgan("dev"));
+  app.use(morgan(config.middelwareloggerFormat));
   app.use(express.urlencoded({ extended: true }));
-
+  
+  // must be after all routes for catch all unmatched routes
+  app.use(notFoundMiddleware);
+  
+  // must be after all routes for catch errors
+  app.use(errorHandlerMiddleware);
+  
   return app;
 }
-
-
-
