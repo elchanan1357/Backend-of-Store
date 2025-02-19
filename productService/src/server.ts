f11aa6838a62a4e849b55586c6b8a3120ecc1d41
Ariel-Dayan <ariel0280@gmail.com>
2025-02-19 09:26:54 +0200

    use public controller

--- productService/src/server.ts
+++ productService/src/server.ts
@@ -3,10 +3,10 @@ import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 
+import { setupProductsRoutes } from "./controllers/products";
 import { config } from "./utils/config";
 import { notFoundMiddleware } from "./middlewares/notFound";
 import { errorHandlerMiddleware } from "./middlewares/errorHandler";
-
 
 export const createHttpServer = () => {
   const app = express();
@@ -20,11 +20,14 @@ export const createHttpServer = () => {
   app.use(morgan(config.middelwareloggerFormat));
   app.use(express.urlencoded({ extended: true }));
   
+  app.use("/products", setupProductsRoutes());
+  
   // must be after all routes for catch all unmatched routes
   app.use(notFoundMiddleware);
   
   // must be after all routes for catch errors
   app.use(errorHandlerMiddleware);
   
+
   return app;
 }
